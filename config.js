c5c70c1a322db8a594a0578206a9d59cc6db15fe
dsc <546328100@qq.com>
2018-08-09 15:14:25 +0800

    .

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongo: {
-        host: process.env.NODE_ENV === 'production' ? 'mongodb://mongodb' : 'mongodb://122.114.31.50:27017',
+        host: process.env.NODE_ENV === 'production' ? 'mongodb://mongodb:27017' : 'mongodb://122.114.31.50:27017',
         dbName: 'FreeCodeCamp'
     }
 };
